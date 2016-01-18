2e874c2c7498ce7fbf2c7abf9990af7a3ab3af4e
rlpacht <rl.pacht@gmail.com>
2016-01-18 10:18:47 -0800

    fixed transitions and styling

--- app/notes/new/controller.js
+++ app/notes/new/controller.js
@@ -1,44 +1,6 @@
 import Ember from 'ember';
 
 const NewNoteController = Ember.Controller.extend({
-  noteText: Ember.computed(function() {
-    var noteText = this.get("text") || "";
-    return noteText;
-  }),
-
-  noteTitle: Ember.computed(function() {
-    var noteTitle = this.get("title") || "";
-    return noteTitle;
-  }),
-
-  // _dateHelper(dateInMilliseconds) {
-
-  // },
-
-  actions: {
-    saveNote() {
-
-      const newNote = this.store.createRecord("note", {
-        title: this.get("noteTitle"),
-        text: this.get("noteText"),
-        lastChanged: new Date()
-      });
-
-      newNote.save();
-      this.setProperties({
-        noteText: "",
-        noteTitle: ""
-      });
-    },
-
-    discardNote() {
-      this.setProperties({
-        noteText: "",
-        noteTitle: ""
-      });
-    }
-  }
-
 });
 
 export default NewNoteController;
